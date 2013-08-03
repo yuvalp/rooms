e568f010dcb60ac44a41c2398a5b8e2c65f4b873
Yuval Pery <yuvalp188@gmail.com>
2013-08-04 01:16:53 +0300

    Updated routes.rb file

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,14 +1,14 @@
 class RoomsController < ApplicationController
 
 	# The "index" method shows all thr rooms in the database.
-	# It sends all the rooms in database to the view file
+	# It sends all the rooms which are in database to the view file
 	def index
 		@rooms = Room.find(:all)
 	end
 
 	def show
 			@room = Room.find_by_id(params[:id])
-			# If the user entered an ID which doesn't exist, redirect to the list of the room
+			# If the user entered to the url an ID which doesn't exist, redirect to the list of the rooms
 			if !@room
    				 redirect_to :action => "index"
 			end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rooms::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   get 'rooms/show/:id' => "rooms#show"
+  get 'rooms/index' => "rooms#index"
   root 'rooms#index'
 
   # Example of regular route:
